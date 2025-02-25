c8f8414720cfccd4879b3bd3ac916adac24af664
Bernardo Brandão <bbbrandao03@gmail.com>
2025-02-25 15:11:18 +0000

    Implemented timer_test_read_config function

--- lab2/timer.c
+++ lab2/timer.c
@@ -40,22 +40,48 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
     return 1;
   }
 
-  uint32_t status;
-  if (sys_inb(TIMER_CTRL, &status) != OK) {
+  uint8_t status;
+  if (util_sys_inb(TIMER_0 + timer, &status) != OK) {
     printf("Error in sys_inb\n");
     return 1;
   }
 
-  *st = (uint8_t) status;
-  printf("status: %x\n", *st);
+  printf("status: %x\n", status);
 
-  return 1;
+  *st = (uint8_t) status;
+
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  union timer_status_field_val conf;
 
-  return 1;
+  switch (field) {
+    case tsf_all:
+      printf("Status byte: 0x%02x\n", st);
+      conf.byte = st;
+    break;
+    case tsf_initial:
+      printf("Initialization mode: %s\n", (st & (BIT(4) | BIT(5))) == TIMER_LSB ? "LSB" :
+      (st & (BIT(4) | BIT(5))) == TIMER_MSB ? "MSB" :
+      (st & (BIT(4) | BIT(5))) == TIMER_LSB_MSB ? "LSB followed by MSB" : "Unknown");
+      conf.in_mode = (st & (BIT(4) | BIT(5)));
+    break;
+    case tsf_mode:
+      printf("Operating mode: %d\n", (st & (BIT(1) | BIT(2) | BIT(3))) >> 1);
+      conf.count_mode = (st & (BIT(1) | BIT(2) | BIT(3)) >> 1);
+    break;
+    case tsf_base:
+      printf("Counting mode: %s\n", (st & TIMER_BCD) ? "BCD" : "Binary");
+      conf.bcd = (st & TIMER_BCD);
+    break;
+    default:
+      printf("Invalid field\n");
+    return 1;
+  }
+
+  timer_print_config(timer, field, conf);
+
+  return 0;
 }
