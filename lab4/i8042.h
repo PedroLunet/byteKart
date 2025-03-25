5bf5bfd7f24e7a3f3bfacf02e1b2a5159118b70b
Mariana Pereira <marianappereira13@gmail.com>
2025-03-25 12:03:32 +0000

    Lab4 Initial Setup

---
+++ lab4/i8042.h
@@ -0,0 +1,32 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+#include <lcom/lcf.h>
+
+/** @defgroup i8042 i8042
+ * @{
+ *
+ * Constants for programming the i8042 Keyboard.
+ */
+
+#define IRQ_KEYBOARD 1
+
+#define ESC_MAKECODE 0x01
+#define ESC_BREAKCODE 0x81
+
+#define PARITY_STATUS BIT(7)
+#define TIMEOUT_STATUS BIT(6)
+#define AUX_MOUSE BIT(5)
+#define INH BIT(4)
+#define INPUT_BUFFER_FULL BIT(1)
+#define OUTPUT_BUFFER BIT(0)
+
+#define OUT_BUFFER 0x60
+#define IN_BUFFER 0x64
+#define READ_COMMAND_BYTE 0x20
+#define KBC_ST_REG 0x64
+#define KBC_CMD_REG 0x64
+
+#define DELAY_US 2000
+
+#endif
---
+++ lab4/mouse.h
@@ -0,0 +1,9 @@
+#ifndef _MOUSE_LAB4_H_
+#define _MOUSE_LAB4_H_
+
+#include "i8042.h"
+#include "i8254.h"
+
+
+
+#endif
