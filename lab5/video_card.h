52b486766caa21641e5724e7871005e81035b964
Mariana Pereira <marianappereira13@gmail.com>
2025-04-13 23:11:44 +0100

    Redefinition of functions according to lab notes

--- lab5/video_card.h
+++ lab5/video_card.h
@@ -9,7 +9,11 @@
 int (change_VBE_mode)(uint16_t mode);
 int (start_VBE_mode)(uint16_t mode);
 int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
-int (vg_draw_line)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
 int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+
+uint16_t get_hres();
+uint16_t get_vres();
+uint8_t get_bits_per_pixel();
 
 #endif
