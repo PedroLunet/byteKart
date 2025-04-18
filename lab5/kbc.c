70bcc8c5e5d6b670f0daef14c7147229e6ac79ee
Mariana Pereira <marianappereira13@gmail.com>
2025-04-18 15:43:06 +0100

    Added new lines for debugging

--- lab5/kbc.c
+++ lab5/kbc.c
@@ -15,14 +15,14 @@ int (write_command_KBC)(uint8_t port, uint8_t command) {
   while (iterations--) {
     int ret = util_sys_inb(KBC_ST_REG, &st);
     if (ret != 0) {
-      printf("Error reading from status register.");
+      printf("Error reading from status register.\n");
       return 1;
     }
 
     if (!(st & INPUT_BUFFER_FULL)) {
       int ret = sys_outb(port, command);
       if (ret != 0) {
-        printf("Error writing to buffer.");
+        printf("Error writing to buffer.\n");
         return 1;
       }
       return ret;
@@ -39,29 +39,29 @@ int (read_output_KBC)(uint8_t port, uint8_t *output, uint8_t mouse) {
   while (iterations--) {
     int ret = util_sys_inb(KBC_ST_REG, &st); // always read before checking errors
     if (ret != 0) {
-      printf("Error reading KBC status.");
+      printf("Error reading KBC status.\n");
       return 1;
     }
 
     if (st & OUTPUT_BUFFER) {
       ret = util_sys_inb(port, output);
       if (ret != 0) {
-        printf("Error reading output.");
+        printf("Error reading output.\n");
         return 1;
       }
 
       if ((st & (PARITY_STATUS | TIMEOUT_STATUS)) != 0) {
-        printf("Parity or timeout error.");
+        printf("Parity or timeout error.\n");
         return 1;
       }
 
       if (mouse && !(st & AUX_MOUSE)) { 
-        printf("Didn't receive output from mouse.");
+        printf("Didn't receive output from mouse.\n");
         return 1;
       }
 
       if (!mouse && (st & AUX_MOUSE)) { 
-        printf("Didn't receive output from keyboard.");
+        printf("Didn't receive output from keyboard.\n");
         return 1;
       }
       return 0;
@@ -73,14 +73,14 @@ int (read_output_KBC)(uint8_t port, uint8_t *output, uint8_t mouse) {
 
 int (kbc_subscribe_int)(uint8_t *bit_no) {
   if (bit_no == NULL) {
-    printf("Null pointer.");
+    printf("Null pointer.\n");
     return 1;
   }
   *bit_no = (uint8_t) hook_id_kbc;
 
   int ret = sys_irqsetpolicy(IRQ_KEYBOARD, (IRQ_REENABLE | IRQ_EXCLUSIVE), &hook_id_kbc);
   if (ret != 0) {
-    printf("Error subscribing keyboard.");
+    printf("Error subscribing keyboard.\n");
     return 1;
   }
   return 0;
@@ -89,7 +89,7 @@ int (kbc_subscribe_int)(uint8_t *bit_no) {
 int (kbc_unsubscribe_int)() {
   int ret = sys_irqrmpolicy(&hook_id_kbc);
   if (ret != 0) {
-    printf("Error unsubscribing keyboard.");
+    printf("Error unsubscribing keyboard.\n");
     return 1;
   }
   return 0;
@@ -98,6 +98,6 @@ int (kbc_unsubscribe_int)() {
 void (kbc_ih)() {
   int ret = read_output_KBC(OUT_BUFFER, &scancode, 0);
   if (ret != 0) {
-    printf("Error reading scancode.");
+    printf("Error reading scancode.\n");
   }
 }
