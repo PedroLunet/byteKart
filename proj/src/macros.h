49732901490f955a081da1ed69c87f33439a1c16
Mariana Pereira <marianappereira13@gmail.com>
2025-04-28 23:20:06 +0100

    Created macros file

---
+++ proj/src/macros.h
@@ -0,0 +1,27 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+// ESC key
+#define ESC_MAKECODE 0x01
+#define ESC_BREAKCODE 0x81
+
+// makecodes for arrow keys
+#define UP_ARROW 0x48
+#define DOWN_ARROW 0X50
+#define LEFT_ARROW 0X4B
+#define RIGHT_ARROW 0X4D
+
+// breakcodes for arrow keys
+#define UP_ARROW_BREAK 0xC8
+#define DOWN_ARROW_BREAK 0XD0
+#define LEFT_ARROW_BREAK 0XCB
+#define RIGHT_ARROW_BREAK 0XCD
+
+// Enter key
+#define ENTER_KEY 0x1C
+#define ENTER_BREAKCODE 0x9C
+
+#define BACKGROUND_COLOR 0x000000
+#define TRANSPARENT 0xFFFFFE
+
+#endif
