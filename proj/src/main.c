12b992de4de77a022bdad597349e121c98638eb5
Mariana Pereira <marianappereira13@gmail.com>
2025-05-02 11:27:55 +0100

    Fix imports

--- proj/src/main.c
+++ proj/src/main.c
@@ -3,17 +3,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "controller/video_card.h"
-#include "controller/i8042.h"
-#include "controller/i8254.h"
-#include "controller/kbc.h"
-#include "controller/mouse.h"
-
 #include "main.h"
-#include "lcom/timer.h"
-#include "sprite.h"
-#include "menu.h"
-#include "macros.h"
+
 
 extern vbe_mode_info_t vbe_mode_info;
 extern uint8_t scancode;
