4065caeb37346e2196bd22d870011676850aca49
Pedro Lunet <pedrolunet@gmail.com>
2025-05-29 15:07:24 +0100

    fixed final result sorting bug

--- proj/src/view/game/game.c
+++ proj/src/view/game/game.c
@@ -472,18 +472,24 @@ static void calculate_final_race_positions(Game *this, RaceResult *results, int 
         int id;
         int lap;
         int segment;
+        float race_time;
+        bool finished;
     } RaceEntry;
     
     RaceEntry entries[MAX_AI_CARS + 1]; 
     int total_entries = 0;
     
+    // Add player entry
     entries[total_entries].score = calculate_race_position_score(this->player.current_lap, this->player.current_road_segment_idx);
     entries[total_entries].name = "Player";
     entries[total_entries].id = 0;
     entries[total_entries].lap = this->player.current_lap;
     entries[total_entries].segment = this->player.current_road_segment_idx;
+    entries[total_entries].race_time = this->player_finish_time;
+    entries[total_entries].finished = this->player_has_finished;
     total_entries++;
 
+    // Add AI car entries
     for (int i = 0; i < this->num_active_ai_cars; ++i) {
         if (this->ai_cars[i]) {
             entries[total_entries].score = calculate_race_position_score(this->ai_cars[i]->current_lap, this->ai_cars[i]->current_road_segment_idx);
@@ -491,13 +497,28 @@ static void calculate_final_race_positions(Game *this, RaceResult *results, int 
             entries[total_entries].id = this->ai_cars[i]->id;
             entries[total_entries].lap = this->ai_cars[i]->current_lap;
             entries[total_entries].segment = this->ai_cars[i]->current_road_segment_idx;
+            entries[total_entries].race_time = this->ai_cars[i]->finish_time;
+            entries[total_entries].finished = this->ai_cars[i]->has_finished;
             total_entries++;
         }
     }
 
     for (int i = 0; i < total_entries - 1; i++) {
         for (int j = 0; j < total_entries - i - 1; j++) {
-            if (entries[j].score < entries[j + 1].score) {
+            bool should_swap = false;
+
+            if (entries[j].finished && entries[j + 1].finished) {
+                should_swap = (entries[j].race_time > entries[j + 1].race_time);
+            }
+
+            else if (!entries[j].finished && entries[j + 1].finished) {
+                should_swap = true;
+            }
+            else if (!entries[j].finished && !entries[j + 1].finished) {
+                should_swap = (entries[j].score < entries[j + 1].score);
+            }
+            
+            if (should_swap) {
                 RaceEntry temp = entries[j];
                 entries[j] = entries[j + 1];
                 entries[j + 1] = temp;
@@ -509,21 +530,14 @@ static void calculate_final_race_positions(Game *this, RaceResult *results, int 
         results[i].position = i + 1;
         if (strcmp(entries[i].name, "Player") == 0) {
             strcpy(results[i].name, "Player");
-            results[i].race_time = this->player_finish_time;
         } else {
             sprintf(results[i].name, "AI Car %d", entries[i].id);
-            // Find the corresponding AI car and get its finish time
-            for (int j = 0; j < this->num_active_ai_cars; j++) {
-                if (this->ai_cars[j] && this->ai_cars[j]->id == entries[i].id) {
-                    results[i].race_time = this->ai_cars[j]->finish_time;
-                    break;
-                }
-            }
         }
         results[i].id = entries[i].id;
         results[i].lap = entries[i].lap;
         results[i].segment = entries[i].segment;
         results[i].score = entries[i].score;
+        results[i].race_time = entries[i].race_time;
     }
     
     *total_results = total_entries;
