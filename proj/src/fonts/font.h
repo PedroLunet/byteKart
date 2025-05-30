e5e9064c5a48b0dee2934072c91161f9049b98b3
darty <140985760+boa-mano@users.noreply.github.com>
2025-05-30 22:07:53 +0100

    Commented fonts folder

--- proj/src/fonts/font.h
+++ proj/src/fonts/font.h
@@ -1,3 +1,9 @@
+/**
+ * @file font.h
+ * @brief Font rendering system for sprite-based text using glyph data from a texture atlas.
+ * 
+ */
+
 #ifndef FONT_H_
 #define FONT_H_
 
@@ -9,6 +15,12 @@
 #include "sprites/sprite.h"
 #include "macros.h"
 
+/**
+ * @struct GlyphData
+ * @brief Represents the metadata for an individual character glyph.
+ *
+ * Contains the position, size, and spacing details for a glyph.
+ */
 typedef struct GlyphData {
     int x;
     int y;
@@ -19,14 +31,43 @@ typedef struct GlyphData {
     int xadvance;
 } GlyphData;
 
+/**
+ * @struct Font
+ * @brief Represents a font loaded from an XPM image and associated metadata.
+ *
+ * Contains the texture atlas as a sprite and the glyph metadata for rendering text.
+ */
 typedef struct Font {
     Sprite *atlas;
     GlyphData glyphs[MAX_FONT_GLYPHS];
     bool glyphsLoaded;
 } Font;
 
+
+/**
+ * @brief Loads a font from an XPM image and font metadata file.
+ *
+ * @param atlas_data XPM map representing the texture atlas.
+ * @param fntFilename Path to the font metadata file.
+ * @return Pointer to the loaded Font object (or NULL on failure).
+ */
 Font *font_load_xpm(xpm_map_t atlas_data, const char *fntFilename);
+
+/**
+ * @brief Frees memory and resources associated with a Font object.
+ *
+ * @param font Font object to destroy.
+ */
 void font_destroy(Font *font);
+
+/**
+ * @brief Retrieves glyph metadata for a specified character.
+ *
+ * @param font Pointer to the Font object.
+ * @param ch Character to retrieve metadata for.
+ * @param data Pointer to a GlyphData structure to populate.
+ * @return true if the glyph data was found, false otherwise.
+ */
 bool font_get_glyph_data(const Font *font, char ch, GlyphData *data);
 
 #endif /* FONT_H_ */
